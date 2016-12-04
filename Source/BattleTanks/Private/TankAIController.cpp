01e4111f4c485caa8b64470bd0814a4c3f9d153f
Ville Välimaa <fearportal@gmail.com>
2016-12-04 20:52:35 +0200

    AIController, Tank C++ asset and AI's now find player location

---
+++ Source/BattleTanks/Private/TankAIController.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTanks.h"
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn)
+	{
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
+	
+	
+	
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *PlayerTank->GetName());
+	}
+
+}
+
+
---
+++ Source/BattleTanks/Public/TankAIController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "AIController.h"
+#include "TankAIController.generated.h" // Must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+private:
+	ATank* GetControlledTank() const;
+	ATank* GetPlayerTank() const;
+	virtual void BeginPlay() override;
+	
+	
+};
